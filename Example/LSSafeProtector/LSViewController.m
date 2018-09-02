8abd9d2636f441385863b9ef6b1c51527fcaf20c
liusong <song.liu@mytogo.com>
2018-09-02 23:32:28 +0800

    修复由于对象地址被释放又被重新使用，导致的误报crash，以及野指针问题

--- Example/LSSafeProtector/LSViewController.m
+++ Example/LSSafeProtector/LSViewController.m
@@ -33,7 +33,6 @@
     
 - (void)viewDidLoad {
     [super viewDidLoad];
- 
 }
 - (IBAction)testStart:(id)sender {
     [self startSafe:YES];
--- LSSafeProtector/Classes/NSObject+KVOSafe.m
+++ LSSafeProtector/Classes/NSObject+KVOSafe.m
@@ -116,6 +116,8 @@ static NSMutableDictionary *KVOSafeDeallocCrashes() {
         [KVOSafeSwizzledClasses() addObject:className];
     }
 }
+
+
 #pragma mark - 被监听的所有keypath 字典
 - (NSMutableArray *)safe_downObservedKeyPathArray{
     NSMutableArray *array = objc_getAssociatedObject(self, _cmd);
@@ -297,7 +299,7 @@ static NSMutableDictionary *KVOSafeDeallocCrashes() {
 -(LSKVOObserverInfo*)safe_canAddOrRemoveObserverWithKeypathWithObserver:(NSObject *)observer keyPath:(NSString*)keyPath context:(void*)context haveContext:(BOOL)haveContext isAdd:(BOOL)isAdd
 {
     if(observer.safe_notNeedRemoveKeypathFromCrashArray==NO){
-        NSString *observerKey=[NSString stringWithFormat:@"%p",observer];
+        NSString *observerKey=LSFormatterStringFromObject(observer);
         NSMutableDictionary *dic=KVOSafeDeallocCrashes()[observerKey];
         NSMutableArray *array=dic[@"keyPaths"];
         __block NSMutableDictionary *willRemoveDic;
@@ -310,6 +312,11 @@ static NSMutableDictionary *KVOSafeDeallocCrashes() {
             }];
             if(willRemoveDic){
                 [array removeObject:willRemoveDic];
+                if (array.count<=0) {
+                    @synchronized(array){
+                        [KVOSafeDeallocCrashes() removeObjectForKey:observerKey];
+                    }
+                }
             }
         }
     }
@@ -386,19 +393,21 @@ static NSMutableDictionary *KVOSafeDeallocCrashes() {
 -(void)safe_KVODealloc
 {
     LSKVOSafeLog(@"%@  safe_KVODealloc",[self class]);
-    NSString *currentKey=[NSString stringWithFormat:@"%p",self];
-    NSMutableDictionary *crashDic=[NSMutableDictionary dictionary];
-    NSMutableArray *array=[NSMutableArray array];
-    crashDic[@"keyPaths"]=array;
-    crashDic[@"className"]=NSStringFromClass([self class]);
-    KVOSafeDeallocCrashes()[currentKey]=crashDic;
-    for (LSKVOObserverInfo *info in self.safe_upObservedArray) {
-        NSMutableDictionary *newDic=[NSMutableDictionary dictionary];
-        newDic[@"targetName"]=info.targetClassName;
-        newDic[@"targetAddress"]=info.targetAddress;
-        newDic[@"keyPath"]=info.keyPath;
-        newDic[@"context"]=[NSString stringWithFormat:@"%p",info.context];
-        [array addObject:newDic];
+    if (self.safe_upObservedArray.count>0) {
+        NSString *currentKey=LSFormatterStringFromObject(self);
+        NSMutableDictionary *crashDic=[NSMutableDictionary dictionary];
+        NSMutableArray *array=[NSMutableArray array];
+        crashDic[@"keyPaths"]=array;
+        crashDic[@"className"]=NSStringFromClass([self class]);
+        KVOSafeDeallocCrashes()[currentKey]=crashDic;
+        for (LSKVOObserverInfo *info in self.safe_upObservedArray) {
+            NSMutableDictionary *newDic=[NSMutableDictionary dictionary];
+            newDic[@"targetName"]=info.targetClassName;
+            newDic[@"targetAddress"]=info.targetAddress;
+            newDic[@"keyPath"]=info.keyPath;
+            newDic[@"context"]=[NSString stringWithFormat:@"%p",info.context];
+            [array addObject:newDic];
+        }
     }
     
     
@@ -426,13 +435,17 @@ static NSMutableDictionary *KVOSafeDeallocCrashes() {
 }
 +(void)safe_dealloc_crash:(NSString*)classAddress
 {
-    NSDictionary *crashDic = KVOSafeDeallocCrashes()[classAddress];
+    //比如A先释放了 然后地址被B使用了，A先调用safe_KVODealloc 会给KVOSafeDeallocCrashes添加值以地址为key，然后系统去移除A的监听，同时B可能又销毁了，又重新赋值了KVOSafeDeallocCrashes的key因为地址相等所以覆盖了，在系统移除A的过程中，会遍历KVOSafeDeallocCrashes字典里的kaypaths，info.target是否相等，此时已经被赋值成B的相关内容了，所以肯定移除不了，然后A走到此方法时，KVOSafeDeallocCrashes里以地址为key的kaypaths数组还有值，导致误报(A还监听着B监听的内容)，赋值KVOSafeDeallocCrashes以地址为kay的字典的时候，导致字典被释放其他地方又使用，导致野指针
+    NSString *currentKey=[NSString stringWithFormat:@"%@-%@",classAddress,NSStringFromClass(self)];
+    NSDictionary *crashDic = KVOSafeDeallocCrashes()[currentKey];
     for (NSMutableDictionary *dic in crashDic[@"keyPaths"]) {
         NSString *reason=[NSString stringWithFormat:@"%@:(%@） dealloc时仍然监听着 %@:%@ 的 keyPath of %@ context:%@",[self class],classAddress,dic[@"targetName"],dic[@"targetAddress"],dic[@"keyPath"],dic[@"context"]];
         NSException *exception=[NSException exceptionWithName:@"KVO crash" reason:reason userInfo:nil]; LSSafeProtectionCrashLog(exception,LSSafeProtectorCrashTypeKVO);
     }
-    [KVOSafeDeallocCrashes() removeObjectForKey:classAddress];
+    [KVOSafeDeallocCrashes() removeObjectForKey:currentKey];
 }
-
+NSString * LSFormatterStringFromObject(id object) {
+    return   [NSString stringWithFormat:@"%p-%@",object,NSStringFromClass([object class])];
+}
 
 @end
