dfe59953dc1494059801bda75e43233c5b55ce72
liusong <song.liu@mytogo.com>
2019-07-04 23:30:52 +0800

    测试2

--- Example/LSSafeProtector/LSAppDelegate.m
+++ Example/LSSafeProtector/LSAppDelegate.m
@@ -19,6 +19,9 @@
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
+    //222
+    
+    
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
